8bcdeeec8da83346bccbabb927719bcce6b0befb
Victoria Gonzalez <vmjgr@Victorias-MacBook-Pro.local>
2018-07-07 09:35:01 -0400

    page setup/admin page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Zomato API key: 61ce992236e7055cfd9a02a68f0ca805
+//function to display restaurants based on zip code
+
+
+// discountapi.com api key: BOXCrbvk 
+//https://discountapi.com/docs 
+
+
+//GoogleMaps API key AIzaSyA-gC8ZZTnj7f7RjmWyPEYVR1QJNiA38r8
